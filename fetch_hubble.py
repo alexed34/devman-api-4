ab21f0a1a16e66936593efa6fabf32eb73aa1fa2
Эдуард <44254815+alexed34@users.noreply.github.com>
2019-11-12 14:00:40 +0000

    Update fetch_hubble.py

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -7,21 +7,25 @@ urllib3.disable_warnings()
 
 def get_img_habbl(name):
     url = f'https://hubblesite.org/api/v3/images/{name}'
-    response = requests.get(url).json()
-    list_id = [i['id'] for i in response]
-    counter_foto = len(list_id)
+    response = requests.get(url)
+    response.raise_for_status()
+    response = response.json()
+    if not response:
+        raise Exception('нет данных в json обьекте')
+
+    photos = [i['id'] for i in response]
+    counter_foto = len(photos)
     path = 'images'
-    if not os.path.exists(path):
-        os.makedirs(path)
-    for id_photo in list_id:
-        url = f'https://hubblesite.org/api/v3/image/{id_photo}'
+    os.makedirs(path, exist_ok=True)
+    for photo in photos:
+        url = f'https://hubblesite.org/api/v3/image/{photo}'
         response = requests.get(url)
         response.raise_for_status()
         response = response.json()
         image_files = response.get('image_files')
         url_photo = f"https:{image_files[-1]['file_url']}"
-        file_extension = url_photo.split('.')[-1]
-        filname = f'{id_photo}.{file_extension}'
+        file_extension = os.path.splitext(url_photo)[1]
+        filname = f'{photo}{file_extension}'
         response = requests.get(url_photo, verify=False)
         response.raise_for_status()
         with open(os.path.join(path, filname), 'wb') as file:
