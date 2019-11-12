7e945a9a00f08e54e3dcaa80daf2adc4d2e66a8d
Эдуард <44254815+alexed34@users.noreply.github.com>
2019-11-12 14:02:32 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,36 +3,36 @@ import os
 from instabot import Bot
 from dotenv import load_dotenv
 
-def list_img(): 
+
+def list_img():
     imgs = os.listdir('images')
-    if  not os.path.exists('image2'):
+    if not os.path.exists('image2'):
         os.makedirs('image2')
     for img in imgs:
-      image = Image.open(f'images/{img}')
-      width = image.width
-      height = image.height
-      cordionate1 = (width - height) / 2
-      cordionate2 = cordionate1 + height
-      coordinates = (cordionate1, 0, cordionate2, height)
-      cropped = image.crop(coordinates)
-      cropped.save(f"image2/n{img}")
+        image = Image.open(f'images/{img}')
+        width = image.width
+        height = image.height
+        cordionate1 = (width - height) / 2
+        cordionate2 = cordionate1 + height
+        coordinates = (cordionate1, 0, cordionate2, height)
+        cropped = image.crop(coordinates)
+        cropped.save(f"image2/n{img}")
 
 
-def load_img_insta(user_name, pass_word): 
+def load_img_insta(user_name, pass_word):
     imgs = os.listdir('image2')
     bot = Bot()
     bot.login(username=user_name, password=pass_word)
     for img in imgs:
         bot.upload_photo(f'image2/{img}')
 
-def main():
-  load_dotenv()
-  user_name = os.environ['username2']
-  pass_word = os.environ['password']
 
-  #list_img()
-  load_img_insta(user_name, pass_word)
+def main():
+    load_dotenv()
+    user_name = os.environ['USERNAME_INSTAG']
+    pass_word = os.environ['PASSWORD_INSTAG']
+    load_img_insta(user_name, pass_word)
 
 
 if __name__ == '__main__':
-  main()
+    main()
