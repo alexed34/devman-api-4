e7fcec9300cdd38caa691181ff36e860317b177d
Эдуард <44254815+alexed34@users.noreply.github.com>
2019-11-23 17:34:39 +0000

    Update fetch_hubble.py

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -4,35 +4,50 @@ import urllib3
 
 urllib3.disable_warnings()
 
-
-def get_img_habbl(name):
-    url = f'https://hubblesite.org/api/v3/images/{name}'
-    response = requests.get(url)
+def get_response(url, name):
+    urll = f'{url}{name}'
+    response = requests.get(urll)
     response.raise_for_status()
-    response = response.json()
+    return response.json()
+
+
+
+def get_img_habbl(response):
     if not response:
-        raise requests.exceptions.HTTPError('нет данных в json обьекте')
-    photos = [i['id'] for i in response]
-    path = 'images'
-    os.makedirs(path, exist_ok=True)
-    for photo in photos:
-        url = f'https://hubblesite.org/api/v3/image/{photo}'
-        response = requests.get(url)
-        response.raise_for_status()
-        response = response.json()
-        image_files = response.get('image_files')
-        url_photo = f"https:{image_files[-1]['file_url']}"
-        file_extension = os.path.splitext(url_photo)[1]
-        filname = f'{photo}{file_extension}'
-        response = requests.get(url_photo, verify=False)
-        response.raise_for_status()
-        with open(os.path.join(path, filname), 'wb') as file:
-            file.write(response.content)
+        raise Exception('нет данных в json обьекте')
+    return [i['id'] for i in response]
+
+def get_photo(photo, path, response):
+    image_files = response.get('image_files')
+    url_photo = f"https:{image_files[-1]['file_url']}"
+    file_extension = os.path.splitext(url_photo)[1]
+    filname = f'{photo}{file_extension}'
+    response = requests.get(url_photo, verify=False)
+    response.raise_for_status()
+    date = {'filname': filname,
+            'response': response}
+    return date
+
+def write_photo(date, path):
+    with open(os.path.join(path, date['filname']), 'wb') as file:
+        file.write(date['response'].content)
+
 
 
 
 def main():
-    get_img_habbl('wallpaper')
+    path = 'images'
+    os.makedirs(path, exist_ok=True)
+    url_images = 'https://hubblesite.org/api/v3/images/'
+    name = 'wallpaper'
+    response = get_response(url_images, name)
+    photos_name = get_img_habbl(response)
+    url_image = 'https://hubblesite.org/api/v3/image/'
+    for photo in photos_name:
+        response = get_response(url_image, photo)
+        date = get_photo(photo, path, response)
+        write_photo(date, path)
+
 
 
 if __name__ == '__main__':
